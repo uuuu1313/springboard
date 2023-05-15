77ce68e8714c0c2b11789d7915a6c0260255b9e9
uuuu1313 <a51903153@gmail.com>
2023-05-15 14:55:49 +0900

    타임리프 템플릿 설정

--- src/main/java/com/koreait/configs/MvcConfig.java
+++ src/main/java/com/koreait/configs/MvcConfig.java
@@ -1,13 +1,20 @@
 package com.koreait.configs;
 
+import com.koreait.configs.interceptors.SiteConfigInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
+
+    // 사이트 설정 유지 인터셉터
+    private final SiteConfigInterceptor siteConfigInterceptor;
 
     @Value("${file.upload.path}")
     private String fileUploadPath;
@@ -22,5 +29,12 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) { // 정적 경로 설정
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:///" + fileUploadPath);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(siteConfigInterceptor)
+                .addPathPatterns("/**");
+
     }
 }
