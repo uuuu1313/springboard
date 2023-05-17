f15bba1a368963facbbfa8e7e5fd4c0c2305cfe0
uuuu1313 <a51903153@gmail.com>
2023-05-17 10:52:21 +0900

    스프링 시큐리티 LoginFailureHandler, LoginSuccessHandelr 작업 중

--- src/main/java/com/koreait/configs/SecurityConfig.java
+++ src/main/java/com/koreait/configs/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.koreait.configs;
 
+import com.koreait.models.member.LoginFailureHandler;
+import com.koreait.models.member.LoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,8 +19,8 @@ public class SecurityConfig {
                 .loginPage("/member/login")
                 .usernameParameter("userId")
                 .passwordParameter("userPw")
-                .defaultSuccessUrl("/")
-                .failureUrl("/member/login")
+                .successHandler(new LoginSuccessHandler())
+                .failureHandler(new LoginFailureHandler())
                 .and()
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
